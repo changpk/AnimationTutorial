c470c8ba2cea966a7cf922e3a3e5f6b217f4c898
changpengkai <x670990709@126.com>
2015-12-28 15:38:36 +0800

    添加delegate文件

---
+++ MoveTurioal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoveTurioal
+//
+//  Created by sinagame on 15/12/24.
+//  Copyright © 2015年 changpengkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
